725fd301105bb7d807c96af8af61e56393cdd0d6
JPNYKW <ilikegamesme@gmail.com>
2020-12-04 01:03:46 +0900

    :seedling: プロジェクト初期化

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const Index = () =>
+  (
+    <>
+      <div>イケてる css もしょーん やっていきたい</div>
+    </>
+  )
+
+ReactDOM.render(<Index />, document.querySelector('#root'))
+
