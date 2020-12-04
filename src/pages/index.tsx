0373712f0398cab294eeab39cf37af5c9dffe442
JPNYKW <ilikegamesme@gmail.com>
2020-12-04 15:18:59 +0900

    :sparkles: Router の実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Hello from './projects/Hello'
 
 const Index = () =>
   (
+    <div>
+      <h2>イケてる css もしょーん やっていきたい</h2>
+    </div>
+  )
+
+const Main = () =>
+  (
     <>
-      <div>イケてる css もしょーん やっていきたい</div>
-      <Hello />
+      <Router>
+        <Link to="/"><button className={'route-btn'}>Top</button></Link>
+        <Link to="/hello"><button className={'route-btn'}>Hello</button></Link>
+        <Route exact path="/" component={Index}></Route>
+        <Route exact path="/hello" component={Hello}></Route>
+      </Router>
     </>
   )
 
-ReactDOM.render(<Index />, document.querySelector('#root'))
+ReactDOM.render(<Main />, document.querySelector('#root'))
 
