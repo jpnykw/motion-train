a7b00dbff3ab3ca108a9488ae01b1712163cc24d
JPNYKW <ilikegamesme@gmail.com>
2020-12-04 15:18:38 +0900

    :recycle: アニメーションを変更

--- src/pages/projects/Hello.tsx
+++ src/pages/projects/Hello.tsx
@@ -2,13 +2,24 @@ import * as React from 'react'
 
 const Hello = () =>
   (
-    <>
+    <div>
+      <div>
       {
-        Array(10).fill().map((_, id) => (
-          <div className={`object motion-${id}`}></div>
+        // Group 1
+        Array(8).fill().map((_, id) => (
+          <div key={id} className={`object motion-g1-${id}`} />
         ))
       }
-    </>
+      </div>
+      <div>
+      {
+        // Group 2
+        Array(8).fill().map((_, id) => (
+          <div key={id} className={`object motion-g2-${7 - id}`} />
+        ))
+      }
+      </div>
+    </div>
   )
 
 export default Hello
